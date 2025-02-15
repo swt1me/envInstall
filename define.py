5e35cc095433f4a971c076dc55aa459b80fb3fc3
swt1me <chp12342021@163.com>
2025-02-15 14:22:06 +0800

    添加了源代码目录的定义

--- define.py
+++ define.py
@@ -1,13 +1,18 @@
-hostingPlatform = "github.com/"
+hostingPlatform = "https://github.com/"
 
 softwareList = \
-    {
+    [
         "git",
         "cmake",
         "clang",
-    }
+    ]
 repoLists = \
-    {
-        "pthom/hello_imgui",
-        "glfw/glfw"
-    }
+    [
+        "pthom/hello_imgui.git",
+        "glfw/glfw.git"
+    ]
+sourceCodeDir = \
+    [
+        "hello_imgui",
+        "glfw"
+    ]
